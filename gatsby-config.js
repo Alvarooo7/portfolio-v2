d408df91bc89d8acb629df1714988f47018e9665
Daniel Santiago <dsantiagomj@Daniels-MacBook-Pro.local>
2020-05-23 19:40:08 -0500

    Update basic project's info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `dsantiagomj`,
+    description: `Daniel Santiago's portfolio, front-end developer`,
+    author: `@dsantiagomj`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
