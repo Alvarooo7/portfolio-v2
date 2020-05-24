83256b7e65d08fce6db5de4b3134e740fb711383
Daniel Santiago Muñoz Jaramillo <dsantiagomj@gmail.com>
2020-05-24 14:21:52 -0500

    Responsive header

---
+++ src/components/drawer.js
@@ -0,0 +1,82 @@
+import { Link } from "gatsby"
+import React from "react"
+
+import styled from "styled-components"
+
+const ResponsiveMenu = styled.ul`
+  align-items: center;
+  background: white;
+  box-shadow: 0 5px 5px 0 rgba(233, 240, 243, 0.5), 0 1px 0 0 #e6ecf8;
+  display: flex;
+  flex-direction: column;
+  height: calc(40vh - 6rem);
+  justify-content: space-evenly;
+  left: 0;
+  list-style: none;
+  margin: ${props => (props.isOpen ? "0 auto" : "-2500px")};
+  position: absolute;
+  top: 5rem;
+  transition: margin 0.5s;
+  width: 100vw;
+  z-index: 2;
+  @media (min-width: 768px) {
+    display: none;
+  }
+`
+
+const ResponsiveItem = styled.li`
+  text-align: center;
+  width: 100vw;
+  &:hover {
+    cursor: pointer;
+    font-weight: 600;
+  }
+`
+
+const MenuItemButton = styled.button`
+  background: transparent;
+  border: solid 2px #1159f6;
+  border-radius: 0.25rem;
+  color: #1159f6;
+  padding: 0.5rem;
+  &:hover {
+    background: #1159f6;
+    color: white;
+    cursor: pointer;
+  }
+`
+
+const Divider = styled.div`
+  background: #eee;
+  height: 1px;
+  margin: 0 auto;
+  width: 90vw;
+`
+
+const Drawer = ({ isOpen }) => (
+  <ResponsiveMenu isOpen={isOpen}>
+    <Link
+      to="/about"
+      style={{
+        color: `black`,
+        textDecoration: `none`,
+      }}
+    >
+      <ResponsiveItem>About</ResponsiveItem>
+    </Link>
+    <Divider />
+    <Link
+      to="/contact"
+      style={{
+        color: `black`,
+        textDecoration: `none`,
+      }}
+    >
+      <ResponsiveItem>
+        <MenuItemButton>Contact</MenuItemButton>
+      </ResponsiveItem>
+    </Link>
+  </ResponsiveMenu>
+)
+
+export default Drawer
--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,117 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+import styled from "styled-components"
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai"
+
+import Drawer from "./drawer"
+
+import LogoImage from "../images/logo_transparent_background.png"
+
+const Container = styled.div`
+  align-items: center;
+  background: white;
+  display: flex;
+  height: 5rem;
+  justify-content: space-between;
+  margin: 0 auto;
+  margin-bottom: 1.45rem;
+  max-width: 60rem;
+  padding: 1.45rem 1.0875rem;
+  width: 100%;
+`
+
+const Menu = styled.ul`
+  display: none;
+  list-style: none;
+  margin: 0;
+  width: 16rem;
+  @media (min-width: 768px) {
+    align-items: center;
+    display: flex;
+    justify-content: space-around;
+  }
+`
+
+const MenuItem = styled.li`
+  font-size: 0.875rem;
+  margin: 0;
+  text-decoration: none;
+  &:hover {
+    cursor: pointer;
+    font-weight: 600;
+  }
+`
+
+const MenuItemButton = styled.button`
+  background: transparent;
+  border: solid 2px #1159f6;
+  border-radius: 0.25rem;
+  color: #1159f6;
+  padding: 0.5rem;
+  &:hover {
+    background: #1159f6;
+    color: white;
+    cursor: pointer;
+  }
+`
+
+const ResponsiveButton = styled.button`
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  display: block;
+  z-index: 3;
+  @media (min-width: 768px) {
+    display: none;
+  }
+`
+
+const Header = () => {
+  // responsive navbar status
+  const [navbarStatus, setNavbarStatus] = useState(false)
+
+  return (
+    <Container>
+      <Link
+        to="/"
+        style={{
+          color: `black`,
+          textDecoration: `none`,
+        }}
+      >
+        <img style={{ margin: 0, zIndex: 3 }} src={LogoImage} width="150px" />
+      </Link>
+      <Menu>
         <Link
-          to="/"
+          to="/about"
           style={{
-            color: `white`,
+            color: `black`,
             textDecoration: `none`,
           }}
         >
-          {siteTitle}
+          <MenuItem>About me</MenuItem>
         </Link>
-      </h1>
-    </div>
-  </header>
-)
+        <Link
+          to="/contact"
+          style={{
+            color: `black`,
+            textDecoration: `none`,
+          }}
+        >
+          <MenuItem>
+            <MenuItemButton>Get in Contact</MenuItemButton>
+          </MenuItem>
+        </Link>
+      </Menu>
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+      <ResponsiveButton onClick={() => setNavbarStatus(!navbarStatus)}>
+        {navbarStatus ? <AiOutlineClose /> : <AiOutlineMenu />}
+      </ResponsiveButton>
 
-Header.defaultProps = {
-  siteTitle: ``,
+      <Drawer isOpen={navbarStatus} />
+    </Container>
+  )
 }
 
 export default Header
