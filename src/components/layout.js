158411a57f29738396e7a88e5e60ae8ea2da67f7
Daniel Santiago Muñoz Jaramillo <dsantiagomj@gmail.com>
2020-05-24 22:39:09 -0500

    layout style update

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,6 +39,7 @@ const Layout = ({ children }) => {
       <div
         style={{
           margin: `0 auto`,
+          marginBottom: 100,
           maxWidth: 960,
           padding: `0 1.0875rem 1.45rem`,
         }}
